c852dfcbdbc74603b3f007d6cda8e90d79ecb977
Aalaa-Fahim <alaahussin.322001@gmail.com>
2024-04-20 01:20:57 +0200

    installs flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,4 @@
-# Installs flask from pip3 version (2.1.0)
+# A puppet manifest installing flask v2.1.0 which is a package from pip3.
 package { 'flask':
   ensure   => '2.1.0',
   provider => pip3,
