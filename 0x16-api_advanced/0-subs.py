5391ac1c64e73f9e7ec0c238fe3d4a3f4dfb0f2f
Aalaa-Fahim <alaahussin.322001@gmail.com>
2024-06-06 20:20:51 +0300

    number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 """
-queries subscribers on a given Reddit "subreddit".
+queries the Reddit API and returns the number of
+subscribers (not active users, total subscribers) for a given subreddit
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """Return the total number of subscribers on a given subreddit"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)'}
+    headers = {'User-Agent':
+               'python:subreddit.subscriber.count:v1.0 (by /u/aalaa_fahim)'}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
             return data['data']['subscribers']
-        return 0
+        else:
+            return 0
     except requests.RequestException:
         return 0
