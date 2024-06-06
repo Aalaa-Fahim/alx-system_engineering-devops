f95f4cec1b24fec024a703e51d61ecbe1e87d851
Aalaa-Fahim <143665741+Aalaa-Fahim@users.noreply.github.com>
2024-06-06 19:51:51 +0300

    Updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)'}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
